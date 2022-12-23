6055caffeeabeaf7ba080399e28a971a66d6b551
Alberto Melián Rodríguez <aeronpsaro@gmail.com>
2022-12-23 12:17:06 +0000

    Arreglado el loop.py

--- loop.py
+++ loop.py
@@ -1,11 +1,21 @@
-executionSpins = S
+from spinTo import spinTo
+from getDistance import getDistance
+from getNorth import getNorth
+from getValleys import getValleys
+from getBestValley import getBestValley
+from getRoute import getRoute
+from move import move
+
+MEASURE_WIDTH = 10
+angles = range(-90,90,MEASURE_WIDTH)
+SPINS = 3
 def loop():
-	measurements = []
-	for i in range(len(angles)):
-		measuredAngle = spinTo(angle[i])
-		measurements.append((measuredAngle, measure()))
-	north = getNorth()
-	valleys = getValleys(measurements)
-	chosenValley = getBestValley(valleys, north)
-	phi = getRoute(chosenValley, north)
-	move(executionSpins, phi)
+    measurements = []
+    for angle in angles:
+        measuredAngle = spinTo(angle)
+        measurements.append((measuredAngle, getDistance()))
+    north = getNorth()
+    valleys = getValleys(measurements)
+    chosenValley = getBestValley(valleys, north)
+    phi = getRoute(chosenValley, north)
+    move(SPINS, phi)
