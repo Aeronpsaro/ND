97bc68089c1db7c092f5cf47917f07fa157e335f
María Naranjo Almeida <41172222+marianaral@users.noreply.github.com>
2022-12-23 11:09:55 +0000

    Fixed bugs and removed threshold condition

--- getRoute.py
+++ getRoute.py
@@ -1,4 +1,3 @@
-threshold = 10 #Tamaño mínimo del valle para que quepa el robot
 minWidthCenter = 25 #Tamaño mínimo del robot para que el robot no tenga que ir por el centro
 securityDist = 5 #Distancia de seguridad que tiene que dejar el robot con respecto a los obstáculos
 
@@ -8,24 +7,19 @@ securityDist = 5 #Distancia de seguridad que tiene que dejar el robot con respec
 # - north: donde se encuentra el objetivo
 #------------------
 def getRoute(valley, north):
-    valleyWidth = valley[1] - valley[0]
-    #Si el ancho del valle es mayor que el threshold, significa que puede caber
-    if (valleyWidth > threshold ):
-        #Si el norte está dentro del valle
-        if (north < valley[1] and north > valley[0]):
-            spinTo(north)
-        else:
-            #Comprobamos que si el ancho no es suficiente, el robot tiene que ir
-            # por el centro del valle
-            if (valleyWidth < minWidthCenter):
-                spinTo(valleyWidth / 2)
-                
-            else:
-                #Comprobamos de que lado del valle está más cerca north
-                if (abs(north - valley[0]) < abs(north - valley[1])):
-                    spinTo(valley[0] + securityDist)
-                else:
-                    spinTo(valley[1] + securityDist)
+    valleyWidth = valley[1][0] - valley[0][0]
+    #Si el norte está dentro del valle
+    if (north < valley[1][0] and north > valley[0][0]):
+        return north
     else:
-        #Si no hay ningún valle donde quepa el robot, que gire 90º
-        spinTo(90)
+        #Comprobamos que si el ancho no es suficiente, el robot tiene que ir
+        # por el centro del valle
+        if (valleyWidth < minWidthCenter):
+            return valleyWidth / 2
+            
+        else:
+            #Comprobamos de que lado del valle está más cerca north
+            if (abs(north - valley[0][0]) < abs(north - valley[1][0])):
+                return valley[0][0] + securityDist
+            else:
+                return valley[1][0] + securityDist
