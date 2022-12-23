4f42c2b08c01f5ecc2e46d79785e8c5e23286978
LeyreRodriguez <91256387+LeyreRodriguez@users.noreply.github.com>
2022-12-23 11:10:59 +0000

    Update bestValleys.py

--- bestValleys.py
+++ bestValleys.py
@@ -1,14 +1,16 @@
-def getBestValley(valleys, north):
+
+def bestValleys(valleys, north):
+    
     result = []
+    dist_seguridad = 10
 
     for i in range(len(valleys)):
-        dif = valleys[i][1] - valleys[i][0] 
-        if north > valleys[i][1] or north < valleys[i][0]:
+        dif = valleys[i][1][0] - valleys[i][0][0]
+        if north > valleys[i][1][0] or north < valleys[i][0][0]:
+            continue
+        elif dif <= dist_seguridad:
             continue
         else:
             result.append((dif, i))
-            
     result.sort(key = lambda x: x[0])
     return valleys[result[0][1]]
-    
-    
